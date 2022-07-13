f1c0eb58a8cf16b8ecbe42f183d8f542ff8dd03f
LeoZion <leozionsuporte@gmail.com>
2022-07-13 16:34:26 -0300

    add product model -mf

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->paragraph(4),
+            'image' => $this->faker->imageUrl($width=640, $height=480),
+            'price' => $this->faker->randomNumber(3)
+        ];
+    }
+}
