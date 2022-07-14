cb3e7be573f74467aa93a2d7b3c667f739e3e4ca
LeoZion <leozionsuporte@gmail.com>
2022-07-13 23:29:56 -0300

    add shoppincart livewire component

---
+++ app/Http/Livewire/ShoppingCart.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\ShoppingCart as Cart;
+use Livewire\Component;
+
+class ShoppingCart extends Component
+{
+    public
+        $cartItems,
+        $sub_total = 0,
+        $total = 0,
+        $discount = 0;
+
+    public function render()
+    {
+        $this->cartItems = Cart::with('product')
+            ->where(['user_id' => auth()->user()->id])
+            ->get();
+
+            $this->sub_total = 0;
+            $this->total = 0;
+            $discount = $this->discount;
+
+            foreach($this->cartItems as $item) {
+                $this->sub_total += $item->product->formatted_price * $item->quantity;
+            }
+
+            $this->total = $this->sub_total - $this->discount;
+
+        return view('livewire.shopping-cart');
+    }
+
+    public function incrementQty($id)
+    {
+        $cart = Cart::whereId($id)->first();
+        $cart->quantity += 1;
+        $cart->save();
+    }
+
+    public function decrementQty($id)
+    {
+        $cart = Cart::whereId($id)->first();
+
+        if ($cart->quantity > 1) {
+            $cart->quantity -= 1;
+            $cart->save();
+        }
+
+
+    }
+
+    public function removeItem($id) {
+        $cart = Cart::whereId($id)->first();
+        $cart->delete();
+    }
+}
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,6 +20,13 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+
+                <x-nav-link :href="route('shoppingcart')" :active="request()->routeIs('shoppingcart')">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                </x-nav-link>
+
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
