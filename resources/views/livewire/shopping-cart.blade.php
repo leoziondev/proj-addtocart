6fd81fff19de7461009440effb6b32f56fff6db5
LeoZion <leozionsuporte@gmail.com>
2022-07-14 00:21:15 -0300

    fix width box

--- resources/views/livewire/shopping-cart.blade.php
+++ resources/views/livewire/shopping-cart.blade.php
@@ -1,5 +1,5 @@
 <div class="flex justify-center my-6">
-    <div class="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
+    <div class="flex flex-col w-full xl:max-w-7xl p-8 text-gray-800 bg-white shadow-lg pin-r pin-y mx-8 xl:mx-0">
         <div class="flex-1">
             <table class="w-full text-sm lg:text-base" cellspacing="0">
                 <thead>
