b2053d87b2709a23139bc7f28d06a2293c06656b
LeoZion <leozionsuporte@gmail.com>
2022-07-13 23:30:10 -0300

    add fnc add to cart

--- app/Http/Livewire/Productlist.php
+++ app/Http/Livewire/Productlist.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Product;
+use App\Models\ShoppingCart;
 use Livewire\Component;
 
 class Productlist extends Component
@@ -20,6 +21,13 @@ class Productlist extends Component
     {
         if (auth()->user()) {
             // add to cart
+            $data = [
+                'user_id' => auth()->user()->id,
+                'product_id' => $id,
+            ];
+
+            ShoppingCart::updateOrCreate($data);
+            session()->flash('success', 'Product added to the cart successfully!');
         } else {
             // redirect to login
             return redirect()->route('login');
